ba871c112fb95b1db782049e2be36e942bfcc414
runaydhaygude <runaydhaygude97@gmail.com>
2025-12-13 04:37:47 +0530

    error handling

---
+++ src/main/java/com/runaydhaygude/graphql/controller/GraphqlExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.runaydhaygude.graphql.controller;
+
+import org.springframework.graphql.execution.ErrorType;
+import graphql.GraphQLError;
+import graphql.GraphqlErrorBuilder;
+import graphql.schema.DataFetchingEnvironment;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GraphqlExceptionHandler extends DataFetcherExceptionResolverAdapter {
+
+    @Override
+    protected GraphQLError resolveToSingleError(Throwable ex, DataFetchingEnvironment env) {
+        ErrorType type= null;
+        if (ex instanceof DataIntegrityViolationException) {
+            type = ErrorType.BAD_REQUEST;
+        } else {
+            type = ErrorType.INTERNAL_ERROR;
+        }
+
+        return GraphqlErrorBuilder.newError(env)
+                .message("Received Message: %s".formatted(ex.getMessage()))
+                .errorType(type)
+                .build();
+    }
+}
